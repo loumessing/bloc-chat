1d6fbcafabd78a02168d8f41063b2a752d4828dd
loumessing <loumessing@gmail.com>
2017-12-05 11:14:47 -0700

    assignment-6-sendmsg:stuck

--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -8,13 +8,17 @@
             return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId)); //the first method takes in the firebaseArray property "roomId", the second the function parameter. It's just saying "If there's a match between the property and the argument, I wanna see it"
             //now that I have $id, can pass into this method to get associated messages.
             //need controller method to set up
-            //getparamaters 
-        };
+            //getparamaters
+            Message.send = function(newMessage) {
+                messages.$add(newMessage);
+                newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+
+        }
 
         return Message;
     }
 
     angular
         .module('blocChat')
-        .factory('Message', ['$firebaseArray', Message]);
+        .factory('Message', ['$firebaseArray', '$cookies', Message]);
 })();
