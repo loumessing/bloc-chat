82f70d4cc8f6a02d2a87c0245e0b058437594a64
loumessing <loumessing@gmail.com>
2018-01-08 09:46:56 -0700

    assignment-6-sendmsg

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,11 +1,14 @@
-(function(){
-  function HomeCtrl(Room, Message, $scope) {
+(function(){//the middle manager between home.html (view) and room.js service
+  function HomeCtrl(Room, Message, $scope, $cookies) {
       /*References Room function which lives inside of Room.js. That function creates an empty Room object, then gets all the children in rooms array, then sets that as the variable rooms, then sets that equal to the method Room.all*/
-      this.title = "Welcome to Bloc Chat!";
       this.room = Room.all; //method created in Room.js to list all chat rooms
       this.currentRoom = null;
-      this.messages = null;
+      this.messages = Message.all;
+      this.userName = $cookies.get('blocChatCurrentUser');
+      //gets user name from text
 //controller method
+      this.title = "Welcome to Bloc Chat!";
+
       this.setRoom = function(room) {
           $scope.currentRoom = room;
           console.log(room);
@@ -14,9 +17,9 @@
       };
 
       this.sendMessage = function(message) {
-        //alert("Hi there");
-        //explain what this did
-        alert(message);
+        Message.send(message, $scope.currentRoom);
+
+        //alert(message);
         //we want to display the actual message as an alert, like an onion!
         //set this up between the data(firebase); how are we accessing the username handle?
         //build the message params/data
@@ -25,6 +28,6 @@
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', 'Message', '$scope', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$scope', '$cookies', HomeCtrl]);
 
 })();
--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -1,19 +1,34 @@
-(function() {
-    function Message($firebaseArray) {
-        var Message = {};
+(function() { //
+    function Message($firebaseArray, $cookies) {
+        var Message = {}; //creates open message object
         var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
+
 
         Message.getByRoomId = function(roomId) {
             return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId)); //the first method takes in the firebaseArray property "roomId", the second the function parameter. It's just saying "If there's a match between the property and the argument, I wanna see it"
             //now that I have $id, can pass into this method to get associated messages.
             //need controller method to set up
             //getparamaters
-            Message.send = function(newMessage) {
-                messages.$add(newMessage);
-                newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+                };
+            Message.send = function(newMessage, currentRoom) {
+                var userName = $cookies.get('blocChatCurrentUser');
+                //passes new message to firebase with the userName
+                var sentAt = firebase.database.ServerValue.TIMESTAMP;
+                //we have the time recorded, so now we need to get all the params in one box
+                var messageObj = {
+                  username: userName,
+                  content: newMessage,
+                  sentAt: sentAt,
+                  roomId: currentRoom.$id
+                  //why is this happening to me?
+                  //you need to build an object with the above params
 
-        }
+                };
+                messages.$add(messageObj);
+                // takes all the messageObj and pushes it as the message
+
+            };
 
         return Message;
     }
